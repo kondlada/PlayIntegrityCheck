8bff578823965554090841f20b8c18e6b830a9f3
kondlada <karthik.kondlada@gmail.com>
2026-09-24 00:04:42 +0530

    Update route.ts

--- app/api/verify-integrity/route.ts
+++ app/api/verify-integrity/route.ts
@@ -6,19 +6,19 @@ import crypto from 'crypto';
 const getSecret = () => process.env.HMAC_SECRET || 'fallback-secret-key-change-in-env';
 
 // Hardcoded GCP credentials
-const GCP_CREDENTIALS = {
-  "type": "service_account",
-  "project_id": "mygintigrity",
-  "private_key_id": "771dc1cb7fc2dc1fe9f57f794973ee546b1a7d0c",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQD5svTgvzQsOlVY\n8ahSf7lQ6DH3KWow+v7hnJyq3QIvLcBRLCesKRP69ekl4RcrLmdFMhxWAQj3PnM1\ncRFnS/TER7KirE6lkV7tIuNB0Fo15Qlg893J0O4MF5XNxlDKdYx/0Nga0X01w/1s\n2n4MH8YDc06MquL7wAMvVuiWoRWKnllegwwQ66BUuYTQLKDms5Qxf8IQk9TOG31G\npeCMCoN2mmmPHh4YaLbgHy+RAFbnRXwqTbIm+HvCdRuzanBzOK5Lbju93pGsdjMH\noKBxDmKeT63KJssBaDwuvQ4xlL6DEBHUdzQWls/c/OWG9PyUlNXk1a4ZabGgkLpo\nborQwl5jAgMBAAECggEAHWsCjl9TJ791olrSOxSrghyLv8lZe/d47klebIOeSBhV\nTJrtpPWv/MSvw76Jqr+MUNWEYMjoGVelCM+kk8LEoxf+s/X7TBFc7tufN6Kdm4o7\njnhyEbQI9P7AWkkk5+gXy0/mbf5usytp4O26+ksHWre2h5HIgo15h+sZevRJI+v5\nb5BGivGSEigXknWYNs9zwagfcFqqyyeNhsNfVwISKAyamMFphYNUcsfADoe44jG1\nX7iWg5jdsPDf9vy/SoXAig6h409k+niSiHvT+36eUVArSFcBjHs2oCz6QjgEUl25\nE7GSMutKnMeZdbDaoJlgsAoFewSum0fkPW8BWQ09QQKBgQD/7s6yIrGAGGwH6xH3\nJrwi9rLhoDV12Dme1NXuguyTJRXCHHTm1ZMyptNtZs3I67URyji4LfPSwIMOyMc1\n+SeenAbC1aT8kU4Icy4UjcEzz9pc2d5WEeqbklQisGGbI3E/V2mEYF/X1svcKg9Q\nELBiZqlHMu4sQv5m8ud5YlBCQQKBgQD5w7r6mGwOyy/ZqDgqc7mqaJCHw9htrSqE\nloICgFeKiatsu+VbQm4YKRZZpL4PSWvnRlwnXLXF/15EmbbdKC0nMBHk2kd3D31u\nblQgleyLxYSK30lnyMGcAvTr84iEn3AdBmrzfkPTn1ugyTYv8KjBidTuZ+7l+gcH\nDWCxM25vowKBgCd+MVUZt2w92IXMR2RtDMEUcnwQnc9Gg3W4AB8jAuCd62Kdw4hP\nUryRM7UBFSY5ASLyopgBN0vBOzgP0XXJscxu5TgxgZkbzv/MzkKLsVGOdHK5h+6e\nKybAiM0ljJpE+Ne+ZGDyAjgrRCAAOYawXJeCogKDoG8iE2HNf9yiSCJBAoGBAMV5\nI5SOfqETtTl57y5hYSKCmgM9rRjKGRvLg+9GWfCG+Kp3xNdqpnEjyzAFOOANROsO\njj3w5ycsyPOkNRiWc1zQfNVcX7Sa4qk1Qf6hiuzXtfu2nusXpzwaelszqXRyOso4\nEMeDxUzEakWt0NIGjOa4e0ojkpbyICxLP2/bIi3RAoGAbSsyqf8L+yNywzrYeYsq\nrMzxfA65jIs0d5T04EnG6MncC8F9znKINa38nITQoHtXqoKucPP1g4SOJIxvs07e\nZpzvgPnhdcUcYJnG0+1MU2vVnxEoUVSmvVqLpqqbX7SU58age2jL1KlAPBwpMvRC\nO8EymShgOFk56Ne24kL3Ryc=\n-----END PRIVATE KEY-----\n",
-  "client_email": "mygoogleplayintigrity@mygintigrity.iam.gserviceaccount.com",
-  "client_id": "115983173969335134851",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mygoogleplayintigrity%40mygintigrity.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-};
+const GCP_CREDENTIALS ={
+                         "type": "service_account",
+                         "project_id": "mygintigrity",
+                         "private_key_id": "dd8cd5b7c5f2252fe6beee2307896abea8bf0953",
+                         "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCzLhdtIu5d9VEm\n67yT4sWfY03hCjBdEwe40Rp+0EKsO3eQdLDxgOAlr8r43CNxZNss+sh7TQ3kdl2B\n3CRikm/Suv+e2vim8+mmdWoSjbD8P1Xm5jc03qHudE3u475oS2kvUqvTv70cxsZd\nEhO/1o3aGsPpAf5Var+eg6hKai8h9ml+YwKYwEqcJz/cWt5z0jUYrvdVgc+AB66j\nc6bN17UW1LbRvWdGZlZteWZWVhdy7oBKLvLYFd/mu4xAUIXSY4a4vErxyJBW53sC\n8KJZWHvFQH+mzF7FwABe+jD2vh6PF/o7pdYHtV/AUY/zXDSYmehnnpr3IPyW9f/1\ntOZGeQsNAgMBAAECggEAA1yAjKt4Fmde9SQSUlqRuPYS8vEH4Txx0sBReNCpzcGR\nKzKxqGm42PiWf9sZXOThW7b9kH4lc1mh/W//dy+0vuHYvKoOuhlJ9nyuEGbRL0Tn\nkU7GTpPvEDBzedMElfj1FBVpuVhtJlhFm7SyxtCj6wz6KBtsPpMVTyvY5LR8Iol+\ntt44eWlgaLKmkw136VEedTkJk32RdGYDTnAmJUFtH+LLs0DQiubd+neyTHICsOxS\nS5ACKbEpiOQQCam2Z3eM60AcFsjK1YKxIL82mHiAxCOrwLrUZQHdbg8MF2Nz9j/u\nw8iprUjh3UDpOsLyrb97UlhTSZz1W9/ni3jkMzb3HQKBgQDXKJ4HB9DU9DelKviw\ngP1aqOMVpmTv+TDyDM13a2Bdvu/6lvqo3CNL9gQaT3DjjLkV1IzAhEN2oXbpSNok\njCM83uTkSFro7N47OqRRVfPzR0gbCLq2908SgpcBC22hqziBCZs6v+T6dDXpOXH2\nlLPuo1tckILQGLAB7RI24f73IwKBgQDVMSK0bhm4XWEZYMq3Kqzrz2JC4lomE0rw\n399TQPY7g95X5s8AjPMzRz20HAYHB3nCrZsU5DE+qoaWztdgFIqROIINDhJSskvL\nUR90LSfkAAT0iFawy0ODwHqF9r71ccPSpu2XMkYOvYVxuGukkjKcWzqwxmVhdpVs\naBiteSYwDwKBgC2z6gVySpDBo4RJIXRUofx8/Wee5bf3xcbUo1xniZvAMDeZlHsD\n79d11Z9FGqSrQrF5acCHF9F+MMUS/Ytf3ShT7Gj1WaI6lmYcacUBLgWSMaXUb9zn\noXs8FBlbtRjvRtxaDwgmtNvijYUR97w6EBJzzrdV1wOfCtwN4J2Z2c/jAoGBAL7n\nAdmpoZRq4bD08HE2YKPED4wu3mSr7/Zjsbn+OL+wZKA3yPMIRBqN1535XhhbRFfk\nVzNM5x/jWSOf0lJx8mb5v9Lq39N3XtGQMec5LDhJICbMEzy6txk6eTyOaA6nMZ/m\n5Gf4l61JXj4n0xQJ6hICyzpSXaP5qPHrAPYNvvlXAoGAID9rRsM4vHuayYL/bz9U\npylP+kyvxfnO94O7RN0lm7Eo2TrO2CYTUC+ghExPEKWZmIf9AJEQR9eT2lMtKfzr\nt6+tFfbkhW1oaQtUn4V3sMK1wS9XmkskivgYa9OwtT0izv5U6ipXpSzNi4r1zY5C\nw9ZVUxd1OWsR5dCV/9AbnmI=\n-----END PRIVATE KEY-----\n",
+                         "client_email": "mygoogleplayintigrity@mygintigrity.iam.gserviceaccount.com",
+                         "client_id": "115983173969335134851",
+                         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                         "token_uri": "https://oauth2.googleapis.com/token",
+                         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                         "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mygoogleplayintigrity%40mygintigrity.iam.gserviceaccount.com",
+                         "universe_domain": "googleapis.com"
+                       };
 
 // GET: Generate/retrieve deterministic HMAC nonce
 export async function GET(request: Request) {
